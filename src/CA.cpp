b51604ebd893ab71a5dc4d402211e39526cd6511
Dylan McClarence <34085068@student.murdoch.edu.au>
2025-09-30 13:21:48 +0800

    Split into helper functions

--- src/CA.cpp
+++ src/CA.cpp
@@ -6,6 +6,13 @@ namespace CellularAutomata
 {
 	uint8_t* outGrid = nullptr;
 	uint8_t* currStateGrid = nullptr;
+
+	namespace Helpers
+	{
+		uint8_t sumNeighbours(const int x, const int y);
+		void evaluateState(uint8_t& cell, const uint8_t currState, const uint8_t numNeighbours);
+		void drawCell(const int x, const int y, const uint8_t alive);
+	}
 }
 
 void CellularAutomata::init(int argc, char** argv)
@@ -40,6 +47,7 @@ void CellularAutomata::displayCallback()
 
 	memcpy(outGrid, currStateGrid, CellularAutomata::width * CellularAutomata::height);
 
+	// Slow down simulation
 	std::this_thread::sleep_for(std::chrono::milliseconds(50));
 
 	// Traverse grid, ignore boundary
@@ -47,37 +55,49 @@ void CellularAutomata::displayCallback()
 	{
 		for (int j = 1; j < CellularAutomata::height - 1; ++j)
 		{
-			uint8_t neighbours = outGrid[(j - 1) * CellularAutomata::width + (i - 1)] + // Top Left	
-								 outGrid[(j - 1) * CellularAutomata::width + i]       + // Top Middle
-								 outGrid[(j - 1) * CellularAutomata::width + (i + 1)] + // Top Right
-								 outGrid[j * CellularAutomata::width + (i - 1)]       + // Middle Left
-								 outGrid[j * CellularAutomata::width + (i + 1)]       + // Middle Right
-								 outGrid[(j + 1) * CellularAutomata::width + (i - 1)] + // Bottom Left
-								 outGrid[(j + 1) * CellularAutomata::width + i]       + // Bottom Middle
-								 outGrid[(j + 1) * CellularAutomata::width + (i + 1)];  // Bottom Right
+			uint8_t neighbours = CellularAutomata::Helpers::sumNeighbours(i, j);
 
+			CellularAutomata::Helpers::evaluateState(currStateGrid[j * CellularAutomata::width + i],
+											         outGrid[j * CellularAutomata::width + i], neighbours);
 
-			if (outGrid[j * CellularAutomata::width + i] == 0x1) // If Alive
-			{
-				currStateGrid[j * CellularAutomata::width + i] = neighbours == 2 || neighbours == 3 ? 0x1 : 0x0;
-				glBegin(GL_POINTS);
-					glColor3f(0.0f, 128.0f, 128.0f);
-					glVertex2i(i, j);
-				glEnd();
-			}
-			else // If Not Alive
-			{
-				currStateGrid[j * CellularAutomata::width + i] = neighbours == 3 ? 0x1 : 0x0;
-				glBegin(GL_POINTS);
-					glColor3f(0.0f, 0.0f, 0.0f);
-					glVertex2i(i, j);
-				glEnd();
-			}
+			CellularAutomata::Helpers::drawCell(i, j, currStateGrid[j * CellularAutomata::width + i]);
 		}
 	}
 
 	glFlush();
 	glutSwapBuffers();
+}
+
+uint8_t CellularAutomata::Helpers::sumNeighbours(const int x, const int y)
+{
+	return outGrid[(y - 1) * CellularAutomata::width + (x - 1)] + // Top Left	
+		   outGrid[(y - 1) * CellularAutomata::width + x]       + // Top Middle
+		   outGrid[(y - 1) * CellularAutomata::width + (x + 1)] + // Top Right
+		   outGrid[y * CellularAutomata::width + (x - 1)]       + // Middle Left
+		   outGrid[y * CellularAutomata::width + (x + 1)]       + // Middle Right
+		   outGrid[(y + 1) * CellularAutomata::width + (x - 1)] + // Bottom Left
+		   outGrid[(y + 1) * CellularAutomata::width + x]       + // Bottom Middle
+		   outGrid[(y + 1) * CellularAutomata::width + (x + 1)];  // Bottom Right
+}
+
+void CellularAutomata::Helpers::evaluateState(uint8_t& cell, const uint8_t state, const uint8_t numNeighbours)
+{
+	if (state == 0x1) // If Alive
+	{
+		cell = numNeighbours == 2 || numNeighbours == 3 ? 0x1 : 0x0;
+	}
+	else // If Not Alive
+	{
+		cell = numNeighbours == 3 ? 0x1 : 0x0;
+	}
+}
+
+void CellularAutomata::Helpers::drawCell(const int x, const int y, const uint8_t alive)
+{
+	glBegin(GL_POINTS);
+	alive == 0x01 ? glColor3f(0.0f, 128.0f, 128.0f) : glColor3f(0.0f, 0.0f, 0.0f);
+	glVertex2i(x, y);
+	glEnd();
 }
 
 void CellularAutomata::run()
